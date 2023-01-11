3690a6b6aec707fc25481af8ded67bd0592e1b5a
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2023-01-11 18:49:01 +0100

    Revert "fit commit"
    This reverts commit 3baa89b0e86b3b48257391b9d3545d733d0d4c07.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import Skills from "./components/Skills";
-import Work from "./components/Work";
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Home />
-      <About />
-      <Skills />
-      <Work />
-      <Contact />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
