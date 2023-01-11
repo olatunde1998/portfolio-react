3baa89b0e86b3b48257391b9d3545d733d0d4c07
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2023-01-11 18:47:25 +0100

    fit commit

---
+++ src/data/data.js
@@ -0,0 +1,38 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+import GogeAfrica from '../assets/projects/goge_africa2.jpeg'
+import FurnitureApp from '../assets/projects/furniture_app2.jpeg'
+
+
+export const data = [
+    {
+        id:1,
+        name:"React JS Application",
+        image:GogeAfrica,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:FurnitureApp,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+
+
+]
