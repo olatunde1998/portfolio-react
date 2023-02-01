83fa2e7083f97871c11958b321bed86fbe46828b
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2023-02-01 23:27:23 +0100

    required was added to input element

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,18 +25,21 @@ const Contact = () => {
           type="text"
           placeholder="Name"
           name="name"
+          required
         />
         <input
           className="my-4 p-2 bg-[#ccd6f6]"
           type="email"
           placeholder="Email"
           name="email"
+          required
         />
         <textarea
           className="bg-[#ccd6f6] p-2"
           name="message"
           rows="10"
           placeholder="Message"
+          required
         ></textarea>
         <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
           Let's Collaborate
